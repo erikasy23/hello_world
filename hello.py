9d529219b7903c43c96f4d49ff01f592001c37f9
erikayoung <erika.samantha.young@gmail.com>
2025-06-11 18:52:26 +0000

    Added a new code segment.

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 print("This is my first Git repository on GitHub.")
+print("Hello Earth!")
+print()
 def print_message(name='GitHub'):
     print(f"Welcome to the {name} tutorial!")
 print_message()
